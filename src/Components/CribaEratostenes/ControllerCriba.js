aeb9d4393eaf8fa040d702d7799dc99022d94ddd
lucianoar <luciano@littlebox.com.ar>
2017-11-13 18:20:52 -0300

    Better validation for number

--- src/Components/CribaEratostenes/ControllerCriba.js
+++ src/Components/CribaEratostenes/ControllerCriba.js
@@ -5,125 +5,140 @@ import Divider from 'material-ui/Divider';
 import Criba from './Criba.js';
 
 class ControllerCriba extends Component {
-  constructor() {
-    super();
-    this.state = {
-      number: '',
-      raiz: '',
-      errorText: '',
-      firstRun: false,
-      running: false,
-      criba: [],
-      m1: 2,
-      m2: 2,
-      answer: '',
-    };
+  state = {
+    number: '',
+    raiz: '',
+    errorText: '',
+    firstRun: false,
+    running: false,
+    disabled: true,
+    criba: [],
+    m1: 2,
+    m2: 2,
+    answer: '',
+  };
 
-    this.delay = 0;
+  delay = 0;
+  max = 2000;
 
-    this.changeNumber = ev => {
-      const value = parseInt(ev.target.value, 10);
-      let state = {};
-      if (value) {
-        state = {
-          number: value,
-          errorText: '',
-        };
-      } else {
-        state = {
-          number: '',
-          errorText: 'Debe ingresar un número',
-        };
-      }
-      this.setState(state);
-    };
+  validateNumber = number => {
+    const value = parseInt(number, 10);
+    if (isNaN(value)) {
+      return {
+        number: '',
+        errorText: 'Debe ingresar un número entero',
+        disabled: true,
+      };
+    } else if (value < 2) {
+      return {
+        number: value,
+        errorText: 'El número debe ser mayor o igual a 2',
+        disabled: true,
+      };
+    } else if (value > this.max) {
+      return {
+        number: this.max,
+        errorText: 'El número es muy grande para generar una animación',
+        disabled: true,
+      };
+    } else {
+      return {
+        number: value,
+        errorText: '',
+        disabled: false,
+      };
+    }
+  };
 
-    this.initCriba = number => {
-      const criba = [{}];
-      for (let i = 2; i <= number; i++) {
-        criba[i] = {
-          value: i,
-          compound: false,
-          prime: false,
-        };
-      }
-      return criba;
-    };
+  changeNumber = ev => {
+    this.setState(this.validateNumber(ev.target.value));
+  };
 
-    this.start = () => {
-      const {number} = this.state;
-      if (!number || number < 2) return;
-      this.setState(
-        {
-          m1: 2,
-          m2: 2,
-          firstRun: true,
-          raiz: Math.floor(Math.sqrt(number)),
-          criba: this.initCriba(number),
-          running: true,
-          answer: '',
-        },
-        this.tick,
-      );
-    };
+  initCriba = number => {
+    const criba = [{}];
+    for (let i = 2; i <= number; i++) {
+      criba[i] = {
+        value: i,
+        compound: false,
+        prime: false,
+      };
+    }
+    return criba;
+  };
 
-    this.cancel = () => {
-      this.setState({
-        running: false,
+  start = () => {
+    const {number} = this.state;
+    if (!number || number < 2) return;
+    this.setState(
+      {
         m1: 2,
         m2: 2,
-      });
-    };
+        firstRun: true,
+        raiz: Math.floor(Math.sqrt(number)),
+        criba: this.initCriba(number),
+        running: true,
+        answer: '',
+      },
+      this.tick,
+    );
+  };
 
-    this.tick = () => {
-      const {m1, m2, number, raiz, running, criba} = this.state;
-      if (!running || m1 > raiz) {
-        let answer = '';
-        if (running) {
-          answer = `El numero ${number}${
-            criba[number].compound ? ' no ' : ''
-          } es primo`;
-        }
-        this.setState({
-          running: false,
-          answer: answer,
-        });
-        return;
-      } else if (m2 > number / m1) {
-        this.setState(
-          state => {
-            state.m1 = state.criba.filter(
-              c => !c.compound && c.value > state.m1,
-            )[0].value;
-            state.m2 = state.m1;
-            state.criba[m1].prime = true;
-            return state;
-          },
-          () => {
-            window.setTimeout(this.tick, this.delay);
-          },
-        );
-      } else if (criba[m1 * m2].compound) {
-        this.setState(state => {
-          state.m2++;
-          return state;
-        }, this.tick);
-      } else {
-        this.setState(
-          state => {
-            const {m1, m2} = state;
-            state.criba[m1 * m2].compound = true;
-            state.criba[m1 * m2].divisor = m1;
-            state.m2 = m2 + 1;
-            return state;
-          },
-          () => {
-            window.setTimeout(this.tick, this.delay);
-          },
-        );
+  cancel = () => {
+    this.setState({
+      running: false,
+      m1: 2,
+      m2: 2,
+    });
+  };
+
+  tick = () => {
+    const {m1, m2, number, raiz, running, criba} = this.state;
+    if (!running || m1 > raiz) {
+      let answer = '';
+      if (running) {
+        answer = `El numero ${number}${
+          criba[number].compound ? ' no ' : ''
+        } es primo`;
       }
-    };
-  }
+      this.setState({
+        running: false,
+        answer: answer,
+      });
+      return;
+    } else if (m2 > number / m1) {
+      this.setState(
+        state => {
+          state.m1 = state.criba.filter(
+            c => !c.compound && c.value > state.m1,
+          )[0].value;
+          state.m2 = state.m1;
+          state.criba[m1].prime = true;
+          return state;
+        },
+        () => {
+          window.setTimeout(this.tick, this.delay);
+        },
+      );
+    } else if (criba[m1 * m2].compound) {
+      this.setState(state => {
+        state.m2++;
+        return state;
+      }, this.tick);
+    } else {
+      this.setState(
+        state => {
+          const {m1, m2} = state;
+          state.criba[m1 * m2].compound = true;
+          state.criba[m1 * m2].divisor = m1;
+          state.m2 = m2 + 1;
+          return state;
+        },
+        () => {
+          window.setTimeout(this.tick, this.delay);
+        },
+      );
+    }
+  };
 
   render() {
     const {
@@ -132,6 +147,7 @@ class ControllerCriba extends Component {
       firstRun,
       criba,
       running,
+      disabled,
       answer,
       m1,
     } = this.state;
@@ -154,7 +170,7 @@ class ControllerCriba extends Component {
           <RaisedButton
             label="Calcular"
             primary={true}
-            disabled={!number || running}
+            disabled={disabled || running}
             onClick={this.start}
           />
           {running && (
